2da7dc081e237cd313641dd2e30e1844a7d2c64a
Gotta_guess_am_all <doesanyoneactuallygiveanemail@email.com>
2025-02-17 00:29:46 -0700

    Twitch IRC access working.

--- gottaguessemall.py
+++ gottaguessemall.py
@@ -9,26 +9,29 @@ from nltk.corpus import words
 nltk.download("words")
 word_list = set(words.words())  # Load words from nltk
 
-# Connect to Twitch IRC
-sock = socket.socket()
-sock.connect((config.SERVER, config.PORT))
-sock.send(f"PASS {config.TOKEN}\n".encode("utf-8"))
-sock.send(f"NICK {config.NICKNAME}\n".encode("utf-8"))
-sock.send(f"JOIN {config.CHANNEL}\n".encode("utf-8"))
-time.sleep(1)  # Give it a second to connect
+def con_to():
+    # Connect to Twitch IRC
+    sock = socket.socket()
+    sock.connect((config.SERVER, config.PORT))
+    sock.send(f"PASS {config.TOKEN}\n".encode("utf-8"))
+    sock.send(f"NICK {config.NICKNAME}\n".encode("utf-8"))
+    sock.send(f"JOIN {config.CHANNEL}\n".encode("utf-8"))
+    time.sleep(1)  # Give it a second to connect
+    return sock
 
 def find_words(letters, length):
     possible_words = (''.join(p) for p in itertools.product(letters, repeat=length))
     return [word for word in possible_words if word in word_list]
 
-# Example: 6-letter words using only 'x', 'o', and 'r'
-print(find_words("thean", 4))
 
 # Send a message
 def send_message(message):
-    for f in message:
-        message = f"PRIVMSG {config.CHANNEL} :{f}\n"
-        sock.send(message.encode("utf-8"))
-        send_message("Hello, Twitch Chat!")  # Example message
-        time.sleep(1)
+    sock = con_to()
+    message = f"PRIVMSG {config.CHANNEL} :{message}\n"
+    sock.send(message.encode("utf-8"))
+
+
+# Example: 6-letter words using only 'x', 'o', and 'r'
+# print(find_words("thean", 4))
+send_message("we playing")  # Example message
 
